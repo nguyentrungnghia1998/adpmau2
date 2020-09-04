ad7dc8c2de7479582b25351ca4bbe9532c22b2b7
nguyentrungnghia1998 <thandongdoinon98@gmail.com>
2020-09-04 08:42:26 +0700

    Ngay 4/9

---
+++ README.md
@@ -0,0 +1,7 @@
+lenh git.txt la file ghi lenh git
+
+Chạy hàm main để tìm được ma trận P theo off policy
+
+File PDF, phần 3 chạy theo thuật toán VI, thuật toán số 5
+
+Gần giống MPC, điều khiển dự báo, nhưng ko biết trước mô hình
--- main.m
+++ main.m
@@ -32,3 +32,25 @@ for k=3:100
     zpre=[upre;ypre];
     Odata(k-2,:)=kron(zpre',zpre');
 end
+ujdata=ukdata;
+ujdatanext=zeros(98,1);
+for j=0:100
+    Vdata=zeros(98,1);
+    for k=3:100
+        unext=ukdata(k:-1:k-1);
+        ynext=ykdata(k:-1:k-1);
+        znext=[unext;ynext];
+        Vdata(k-2)=ykdata(k)'*Qd*ykdata(k)+ujdata(k)'*Rd*ujdata(k)+gamma*znext'*Pd_*znext;
+    end
+    Pnewstk=pinv(Odata)*Vdata;
+    Pnew=reshape(Pnewstk,[4 4]);
+    if norm(Pnew-Pd_,1)<0.0005
+        break;
+    end
+    for k=3:100
+        upre1=ukdata(k-1);
+        ynext=ykdata(k:-1:k-1);
+        ujdata(k-2)=-1/(Rd/gamma+Pnew(1,1))*(Pnew(1,2)*upre1+Pnew(1,3:4)*ynext);
+    end
+    Pd_=Pnew
+end
